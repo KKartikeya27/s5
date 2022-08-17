97231af47bd10af8b599a49d421f5f96c40c5162
Kartikeya Kulkarni <kartikeya_b200389ec@nitc.ac.in>
2022-08-17 22:19:56 +0530

    3.cpp added

---
+++ 16-8-22/3.cpp
@@ -0,0 +1,20 @@
+//Fibonacci numbers till n
+#include<iostream>
+using namespace std;
+int fibb(int);
+int main()
+    {
+        int n;
+        cout<<"Enter an integer value: ";
+        cin>>n;
+        fibb(n);
+
+        return 0;
+    }
+
+int fibb(int x)
+    {
+        
+
+
+    }
