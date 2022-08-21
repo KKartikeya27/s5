5037ad4dd2cba3f29150a891104a23772ef133b2
KK27 <kartikeya_b200389ec@nitc.ac.in>
2022-08-21 12:37:41 +0530

    hanoi

--- Assignment-1 Part-2/3.cpp
+++ Assignment-1 Part-2/3.cpp
@@ -1,20 +1,16 @@
-//Fibonacci numbers till n
+// Using recursion, write a program which calculates 
+// the Fibonacci series for any n
+
+// 0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610....
+//   1,2,3,4,5,6, 7, 8, 9,10,11, 12, 13, 14, 15.....
 #include<iostream>
 using namespace std;
-int fibb(int);
+
 int main()
     {
         int n;
-        cout<<"Enter an integer value: ";
+        cout<<"Enter number of terms of Fibonacci series required: "<<endl;
         cin>>n;
-        fibb(n);
-
+        Fibtilln(n);
         return 0;
-    }
-
-int fibb(int x)
-    {
-        
-
-
     }
