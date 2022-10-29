410f65ed16b16c554b5b2e1859c9645467b05acf
KKartikeya27 <kartikeya.kulkarni@gmail.com>
2022-10-29 12:29:43 +0530

    Implemented LL append& print

--- Assignment-StacksQuesLLTrees/linkedlistagain.cpp
+++ Assignment-StacksQuesLLTrees/linkedlistagain.cpp
@@ -15,37 +15,50 @@ class LinkedList
             {
                 Node* temp=new Node;
                 temp->data=a;
-                Node*traverse=Head;
-                while(traverse!=NULL)
+                Node* traverse=Head;
+                if(Head==NULL)
                     {
-                        traverse=traverse->next;
+                        Head=temp;
+                        temp->next=NULL;
+                        traverse=Head;
                     }
-                traverse->next=temp;
-                temp->next=NULL;
+                else
+                    {
+                        while(traverse->next!=NULL)
+                            {
+                                traverse=traverse->next;
+                            }
+                        traverse->next=temp;
+                        temp->next=NULL;
+                    }
             }
         void print()
          {
-                Node*traverse=Head;
-                while(traverse!=NULL)
+                Node* traverse=Head;
+                while(traverse->next!=NULL)
                     {
                         cout<<traverse->data<<endl;
                         traverse=traverse->next;
                     }
-
+                cout<<traverse->data<<endl;
             }
     };
 int main()
     {
-        cout<<"enter number of elements to add:";
         int n,a;
-        LinkedList LL;
+        cout<<"enter number of elements to add:";
         cin>>n;
+        LinkedList LL;
         for (int i = 0; i < n; i++)
         {
             cin>>a;
             LL.append(a);
 
         }
+        LL.append(1);
+        LL.append(3);
+        LL.append(5);
+        LL.append(6);
         LL.print();
         
     }
